cca42842bd38498bde12bf8fab588d1bc6860cac
IacobZanoci <114911332+IacobZanoci@users.noreply.github.com>
2024-10-31 18:29:15 +0200

    [FEATURE]: Built Monthly Spending View (by categories)

---
+++ Money/Core/Components/SpendingCategoryCard/SpendingCategoryCardView.swift
@@ -0,0 +1,18 @@
+//
+//  SpendingCategoryCardView.swift
+//  Money
+//
+//  Created by Iacob Zanoci on 31/10/2024.
+//
+
+import SwiftUI
+
+struct SpendingCategoryCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SpendingCategoryCardView()
+}
